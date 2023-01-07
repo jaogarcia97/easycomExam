8fb398577022cf252dc9ee559561ae82205c7805
Jao Garcia <josejaimefelixgarcia@gmail.com>
2023-01-07 16:18:41 +0800

    blank project

---
+++ easycomExam/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  easycomExam
+//
+//  Created by Jao Garcia on 1/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
